4a1db975b301224f3043a5fc166910ddb2bd8740
slavik <relogn@mail.com>
2018-03-17 00:56:41 +0200

    Add product

--- src/modules/handlers/add.js
+++ src/modules/handlers/add.js
@@ -1,11 +1,16 @@
-module.exports = (ctx) => {
+const pageParse = require('../pageParse');
+
+module.exports = async (ctx) => {
   // console.log(ctx.update.message.entities);
-  console.log('commands', ctx.state.command);
-  console.log('commands', ctx.state.args);
-  const args = ctx.state.commands.args;
+  console.log('args', ctx.state.command.args);
+  const args = ctx.state.command.args;
 
   if (args && args.length) {
     const url = args[0];
+    const info = await pageParse(url);
+
+    console.log(info);
   }
+
   ctx.reply('Response from handler');
 }
