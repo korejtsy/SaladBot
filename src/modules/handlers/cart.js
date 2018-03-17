eadb05c43b4c91493dda59038b3cca226937898a
slavik <relogn@mail.com>
2018-03-17 14:57:53 +0200

    Some fixes

--- src/modules/handlers/cart.js
+++ src/modules/handlers/cart.js
@@ -7,16 +7,21 @@ module.exports = async (ctx) => {
   const user = ctx.update.message.from;
 
   const order = await getOrder(chatID);
-  const items = filter(order.items, item => item.user.telegram_account_id == user.id);
 
-  let md = `[@${user.first_name} ${user.last_name || ''}](tg://user?id=${user.id}) ${items.length === 0 ? 'has no' : ''} *items*
-  
+  if (order) {
+    const items = filter(order.items, item => item.user.telegram_account_id == user.id);
+
+    let md = `[@${user.first_name} ${user.last_name || ''}](tg://user?id=${user.id}) ${items.length === 0 ? 'has no' : ''} *items*
+    
 `;
 
-  forEach(items, item => {
-    md += `${item.url} ${item.mod ? `(${item.mod})` : ''}`;
-  });
+    forEach(items, item => {
+      md += `${item.url} ${item.mod ? `(${item.mod})` : ''}`;
+    });
 
-  ctx.replyWithMarkdown(md);
+    ctx.replyWithMarkdown(md);
+  } else {
+    ctx.replyWithMarkdown(`No order in progress`);
+  }
 }
 
--- src/modules/handlers/updateMode.js
+++ src/modules/handlers/updateMode.js
@@ -4,7 +4,8 @@ const editItem = require('../store/editItem');
 
 module.exports = async (ctx) => {
   try {
-    const [id, mod, price] = ctx.callbackQuery.data.split('-');
+    const [id, mod] = ctx.callbackQuery.data.split('-');
+    console.log('price', ctx.callbackQuery.data);
     await editItem(id, { mod });
 
     ctx.reply(`Mod "${mod}" has been set`);
